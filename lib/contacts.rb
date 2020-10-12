381a83c3e2822840b85d2cfcc7dfe278c04011f7
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-12 00:50:19 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,10 +18,9 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contacts.each do |person, contact_details_hash|
       if person == "Freddy Mercury"
-        binding.pry
         contact_details_hash.each do |attribute, data|
           if attribute == :favorite_ice_cream_flavors
-
+            data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
   end
